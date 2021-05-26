1502607e4b10be2effb1f48cb4c27ea4a484ab1a
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-25 23:17:18 -0500

    feat: added click functionality

---
+++ js/personal_site.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$('#pizza').click(() => {
+    window.location = ''
+});
+
+$('#coffee').click(() => {
+    window.location = 'https://shan-mar-codeup.github.io/';
+});
+
+$('#weather-project').click(() => {
+    window.location = 'https://shanshan-su.github.io/weather-project/';
+});
+
+$('#vet-connect').click(()  => {
+    window.location = 'https://veteran-codeathon-marco.github.io/';
+});
+
